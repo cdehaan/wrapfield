49efc5d9c79ef0ec3a1a62525e73c753fa990f67
Chris DeHaan <cdehaan@gmail.com>
2023-06-17 17:50:28 +0900

    renamed "playerSecret" to "secret" when passing to Create and Join PHP

--- CreateBoard.php
+++ CreateBoard.php
@@ -132,9 +132,9 @@
 
     // If the player sent credentials, verify them
     $existingPlayer = false;
-    if(isset($playerInput->playerKey) && isset($playerInput->playerSecret)) {
+    if(isset($playerInput->playerKey) && isset($playerInput->secret)) {
         $playerKey = intval($playerInput->playerKey);
-        $playerSecret = substr(preg_replace("/[^A-Za-z0-9]/", '', $playerInput->playerSecret), 0, 20);
+        $playerSecret = substr(preg_replace("/[^A-Za-z0-9]/", '', $playerInput->secret), 0, 20);
 
         $sql = "SELECT COUNT(*) FROM player WHERE player_key = $playerKey AND secret = '$playerSecret';";
         $result = $conn->query($sql);
