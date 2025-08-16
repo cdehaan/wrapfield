27aad82f3e299c204ce7d90900b4d1f03cdb04ed
Chris DeHaan <chris@nextvision.co.jp>
2025-08-16 13:28:47 +0900

    Small HTML and spacing changes

--- src/WelcomeScreen.tsx
+++ src/WelcomeScreen.tsx
@@ -8,21 +8,19 @@ import JoinBoard from "./JoinBoard";
 function WelcomeScreen({ boardData, myData, competitors, setMyData, setBoardData, setCompetitors}: { boardData:Board, myData:Player, competitors:Player[], setMyData:React.Dispatch<React.SetStateAction<Player>>, setBoardData:React.Dispatch<React.SetStateAction<Board>>, setCompetitors:React.Dispatch<React.SetStateAction<Player[]>> }) {
   if(boardData.active === true) return null;
 
-    return(
-        <>
-          <div className="Welcome">
-            <div className='title'>
-              <span className='title'>Wrapfield</span>
-              <span className='subtitle'>Realtime Multiplayer Minesweeper</span>
-            </div>
-            <div className='logo'></div>
-            <Scoreboard boardData={boardData} competitors={competitors} myData={myData} setMyData={setMyData}/>
-            <CreateBoard active={boardData.active} myData={myData} setBoardData={setBoardData} setMyData={setMyData} />
-            <JoinBoard   active={boardData.active} myData={myData} setBoardData={setBoardData} setMyData={setMyData} setCompetitors={setCompetitors} />
-            <span className='footer'><a href="https://github.com/cdehaan/wrapfield" style={{ color: '#369' }}>By Chris DeHaan</a></span>
-          </div>
-        </>
-      )
+  return(
+    <div className="Welcome">
+      <div className='title'>
+        <span className='title'>Wrapfield</span>
+        <span className='subtitle'>Realtime Multiplayer Minesweeper</span>
+      </div>
+      <div className='logo' />
+      <Scoreboard boardData={boardData} competitors={competitors} myData={myData} setMyData={setMyData}/>
+      <CreateBoard active={boardData.active} myData={myData} setBoardData={setBoardData} setMyData={setMyData} />
+      <JoinBoard   active={boardData.active} myData={myData} setBoardData={setBoardData} setMyData={setMyData} setCompetitors={setCompetitors} />
+      <span className='footer'><a href="https://github.com/cdehaan/wrapfield" style={{ color: '#369' }}>By Chris DeHaan</a></span>
+    </div>
+  );
 }
 
 export default WelcomeScreen;
